74a4608b4ab4afd9feae295d7214b1da04473263
Dierfeus <zulin.zulin15@gmail.com>
2025-12-10 11:22:48 +0700

    test db in github pages

--- js/db.js
+++ js/db.js
@@ -1,32 +1,29 @@
-// db.js
-let db;
-
 function openDB() {
     return new Promise((resolve, reject) => {
-        const request = indexedDB.open("EducationDB", 1);
+        const request = indexedDB.open("myDatabase", 1);
 
-        request.onupgradeneeded = function (event) {
-            db = event.target.result;
+        request.onupgradeneeded = (event) => {
+            const db = event.target.result;
 
-            if (!db.objectStoreNames.contains("users")) {
-                const usersStore = db.createObjectStore("users", { keyPath: "username" });
-                usersStore.createIndex("email", "email", { unique: true });
-            }
-
+            // Создаём хранилище заявок
             if (!db.objectStoreNames.contains("requests")) {
                 const requestStore = db.createObjectStore("requests", { keyPath: "id", autoIncrement: true });
                 requestStore.createIndex("username", "username", { unique: false });
-                 reqStore.createIndex("status", "status", { unique: false });
+            }
+
+            // Создаём хранилище пользователей
+            if (!db.objectStoreNames.contains("users")) {
+                const userStore = db.createObjectStore("users", { keyPath: "username" });
             }
         };
 
-        request.onsuccess = function (event) {
-            db = event.target.result;
+        request.onsuccess = (event) => {
+            const db = event.target.result;
             resolve(db);
         };
 
-        request.onerror = function () {
-            reject("Ошибка при открытии базы");
+        request.onerror = (event) => {
+            reject(event.target.error);
         };
     });
 }
